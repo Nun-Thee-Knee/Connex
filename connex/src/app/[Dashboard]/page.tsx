362242f7605667a15315e2f4a9ca4d6488262b70
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-10 10:34:41 -0700

    Removed the conflicts

--- connex/src/app/[Dashboard]/page.tsx
+++ connex/src/app/[Dashboard]/page.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
     const {data:userData, isLoading} = api.user.getRole.useQuery({id})
   return (
     <div>Dashboard
-        {!isLoading?<h1>Loading</h1>:<p>{JSON.stringify(userData)}</p>}
+        {isLoading?<h1>Loading</h1>:<p>{JSON.stringify(userData)}</p>}
     </div>
   )
 }
