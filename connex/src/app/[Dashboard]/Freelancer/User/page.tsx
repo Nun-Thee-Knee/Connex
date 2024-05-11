86ccda39ee1e4e95093bc440ad060195fbccdf04
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-11 13:07:43 -0700

    Resolved the key conflict 3

--- connex/src/app/[Dashboard]/Freelancer/User/page.tsx
+++ connex/src/app/[Dashboard]/Freelancer/User/page.tsx
@@ -4,7 +4,7 @@ import AvatarUploadPage from "~/components/VercelBlob";
 import { usePathname } from "next/navigation";
 
 const User = async () => {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+  // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
   const id = usePathname().split("/")[1] as string;
   return (
     <div className="text-white">
