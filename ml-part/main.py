1508ea99a3e670cc9f78deb7dea128a065d185b7
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-05-12 01:40:08 +0530

    removed bugs from the deployment in flask restfull api

--- ml-part/main.py
+++ ml-part/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import pdfplumber
+from flask_cors import CORS, cross_origin
+import subprocess
 
 app = Flask(__name__)
+
 
 def extract_text_from_pdf(file_path):
     with pdfplumber.open(file_path) as pdf:
@@ -18,12 +21,14 @@ def home():
 def get_score():
     result = subprocess.run(['python', 'score.py'], stdout=subprocess.PIPE)
     score = result.stdout.decode('utf-8').strip()
-    return jsonify({'score':score})
+    return jsonify({'score': score})
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         pdf_file = request.files['pdf_file']
+        score_result = get_score()
+        ats_score = score_result.json()['score']
         if pdf_file:
             text = extract_text_from_pdf(pdf_file)
             return render_template('index.html', ats_score=ats_score)
