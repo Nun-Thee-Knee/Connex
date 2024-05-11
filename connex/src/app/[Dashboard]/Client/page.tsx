c09d35fcd39018a6b0c00448eaf8967a379184e7
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-11 02:55:06 -0700

    Added the JobForm 2

--- connex/src/app/[Dashboard]/Client/page.tsx
+++ connex/src/app/[Dashboard]/Client/page.tsx
@@ -4,6 +4,7 @@ import { getServerAuthSession } from '~/server/auth'
 
 const page = async() => {
   const session = await getServerAuthSession();
+  // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
   const id = session?.user.id as string;
   return (
     <div className="flex items-center justify-center h-[100vh]">
