5d2afa3bc485b07047162083b27a8f739a2615f5
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-11 00:16:11 -0700

    Added the auth connection

--- connex/src/app/[Dashboard]/page.tsx
+++ connex/src/app/[Dashboard]/page.tsx
@@ -1,16 +1,19 @@
 'use client'
 import React, { use } from 'react'
-import { usePathname} from 'next/navigation'
+import { redirect, usePathname} from 'next/navigation'
 import { api } from '~/trpc/react'
 import { Role } from '@prisma/client'
 import RoleAuth from '~/components/RoleAuth'
 
 const Dashboard = () => {
+  //@ts-ignore
     const id = usePathname().split("/")[1]!;
     const {data:userData, isLoading} = api.user.getRole.useQuery({id})
   return (
     <div>
-      {isLoading?<h1>Loading</h1>:<p>{userData?.role === "None"?(<RoleAuth id={id}/>):(<h1>{userData?.role}</h1>)}</p>}
+      {isLoading?<h1>Loading</h1>:<p>{userData?.role === "None"?(<RoleAuth id={id}/>):(<>
+      {redirect(`/${id}/${userData?.role}`)}
+      </>)}</p>}
     </div>
   )
 }
