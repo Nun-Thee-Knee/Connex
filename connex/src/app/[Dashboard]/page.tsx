f631c9d61e82b7e20b9e74c24899380495275727
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-11 02:12:57 -0700

    Removed the conflicts

--- connex/src/app/[Dashboard]/page.tsx
+++ connex/src/app/[Dashboard]/page.tsx
@@ -1,13 +1,19 @@
 "use client";
-import React, { use } from "react";
+import React, { FC, use } from "react";
 import { redirect, usePathname } from "next/navigation";
 import { api } from "~/trpc/react";
-import { Role } from "@prisma/client";
 import RoleAuth from "~/components/RoleAuth";
 
-const Dashboard = () => {
-  const id = usePathname().split("/")[1]!;
-  const { data: userData, isLoading } = api.user.getRole.useQuery({ id });
+interface dashboardProps {
+  id: string;
+}
+
+const Dashboard: FC<dashboardProps> = () => {
+  const path = usePathname();
+  const id = path?.substring(1);
+  if (id) {
+    const { data: userData, isLoading } = api.user.getRole.useQuery({ id });
+  
   return (
     <div>
       {isLoading ? (
@@ -23,6 +29,7 @@ const Dashboard = () => {
       )}
     </div>
   );
+}
 };
 
 export default Dashboard;
