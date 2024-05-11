c4c5c8287585e0be3fa2d28f5889552952397755
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-05-11 19:59:51 +0530

    resume uploaded

--- connex/src/app/[Dashboard]/Freelancer/page.tsx
+++ connex/src/app/[Dashboard]/Freelancer/page.tsx
@@ -1,14 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import ResumeUpload from "~/components/ResumeUpload";
 
 const page = () => {
   return (
     <div>
+      <ResumeUpload />
       <Link href="/api/auth/signout">
-        <button className='bg-red-900 hover:bg-red-700 p-3 rounded-xl'>Sign Out</button>
+        <button className="rounded-xl bg-red-900 p-3 hover:bg-red-700">
+          Sign Out
+        </button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
