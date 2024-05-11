ad84f60907e7e98b0015aaeabefe3ee5175d87a0
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-05-11 21:57:34 +0530

    changed schema and added cloud for upload files

---
+++ connex/src/app/[Dashboard]/Freelancer/User/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AvatarUploadPage from "~/components/VercelBlob";
+
+const User = () => {
+  return (
+    <div>
+      <AvatarUploadPage />
+    </div>
+  );
+};
+
+export default User;
