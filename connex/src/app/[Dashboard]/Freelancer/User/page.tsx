7dd4ae5a066338aa08777b322f4eb881860e04e5
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-11 13:35:54 -0700

    Added the changes

--- connex/src/app/[Dashboard]/Freelancer/User/page.tsx
+++ connex/src/app/[Dashboard]/Freelancer/User/page.tsx
@@ -4,11 +4,11 @@ import AvatarUploadPage from "~/components/VercelBlob";
 import { usePathname } from "next/navigation";
 
 const User = async () => {
-  // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
-  const id = usePathname().split("/")[1] as string;
+  // const id = usePathname().split("/")[1] as string;
   return (
     <div className="text-white">
-      <AvatarUploadPage id={id}/>
+      {/* <AvatarUploadPage id={id}/> */}
+      <h1>Hello</h1>
     </div>
   );
 };
