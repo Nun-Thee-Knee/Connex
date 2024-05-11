63d56765ea8497a35d88587f35602e7df11cafa6
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-11 12:41:06 -0700

    Resolved the key conflict 3

--- connex/src/app/[Dashboard]/Freelancer/User/page.tsx
+++ connex/src/app/[Dashboard]/Freelancer/User/page.tsx
@@ -4,8 +4,8 @@ import { getServerAuthSession } from "~/server/auth";
 
 const User = async () => {
   const session = await getServerAuthSession();
-  // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
-  const id = session?.user?.id! as string;
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+  const id = await session?.user?.id as string;
   return (
     <div>
       <AvatarUploadPage id={id} />
