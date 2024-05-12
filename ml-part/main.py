5f586b3ce2fa97eaea6c2e33a1660a17b9ac0b45
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-11 17:31:06 -0700

    removed bugs

--- ml-part/main.py
+++ ml-part/main.py
@@ -1,34 +1,19 @@
-from flask import Flask, render_template, request, jsonify
-import pdfplumber
-import subprocess
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-def extract_text_from_pdf(file_path):
-    with pdfplumber.open(file_path) as pdf:
-        text = ""
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
+@app.route("/")
+def main():
+    return "Hello"
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/uploads", methods=["GET", "POST"])
+def uploadPDF():
+    if request.method == "POST":
+        link = request.get_json()
+        return jsonify(link)
+    return "Not A Post Request"
 
-@app.route('/score', methods=['GET'])
-def get_score():
-    result = subprocess.run(['python', 'score.py'], stdout=subprocess.PIPE)
-    score = result.stdout.decode('utf-8').strip()
-    return jsonify({'score':score})
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        pdf_file = request.files['pdf_file']
-        if pdf_file:
-            text = extract_text_from_pdf(pdf_file)
-            return render_template('index.html', ats_score=ats_score)
-    return render_template('index.html', message='Please upload a PDF file.')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
